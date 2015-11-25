462aa2cd6a69f04e28145ce2a0b107c59b36e31c
Tania W Yu <taniayu@mit.edu>
2015-11-25 11:51:44 -0500

    Obstacles mostly working

---
+++ project/project.srcs/sim_1/new/test_bram.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2015 09:34:52 PM
+// Design Name: 
+// Module Name: test_bram
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_bram;
+    reg clock;
+endmodule
---
+++ project/project.srcs/sources_1/bd/design_1/hdl/design_1.v
@@ -0,0 +1,16 @@
+//Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2014.4.1 (lin64) Build 1149489 Thu Feb 19 16:01:47 MST 2015
+//Date        : Wed Nov 18 21:09:28 2015
+//Host        : eecs-digital-24 running 64-bit Ubuntu 12.04.5 LTS
+//Command     : generate_target design_1.bd
+//Design      : design_1
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1
+   ();
+
+
+endmodule
